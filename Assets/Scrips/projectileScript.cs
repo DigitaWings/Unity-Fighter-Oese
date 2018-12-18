e4dee98d6634e33c069b450d80e379f0ec811bac
DigitaWings <peter@slessing.dk>
2018-12-18 19:26:27 +0100

    lol

---
+++ Assets/Scrips/projectileScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileScript : MonoBehaviour
+{
+
+    public float speed;
+    public float lifeTime;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+    void DestroyProjectile()
+    {
+        Object.Destroy(this.gameObject);
+    }
+}
